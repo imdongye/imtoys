2087721b953a551d59f9c47bd21e488239b159f7
songjo35 <manjaro999@ajou.ac.kr>
2022-07-20 18:53:55 +0900

    done program.h

--- Cloth-Simplification/lim/model.h
+++ Cloth-Simplification/lim/model.h
@@ -9,17 +9,13 @@
 #include <glad/glad.h> 
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 #include <stb_image.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-#include <string>
-#include <fstream>
-#include <sstream>
 #include <iostream>
-#include <map>
+#include <string>
 #include <vector>
 
 #include "program.h"
