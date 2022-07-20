efc8cf3469aeb95b682229d96b06f000789e9a2f
songjo35 <manjaro999@ajou.ac.kr>
2022-07-20 13:32:07 +0900

    before pull

---
+++ Cloth-Simplification/lim/program.h
@@ -0,0 +1,13 @@
+
+
+
+#ifndef program_h
+#define program_h
+
+struct Program {
+public :
+	unsigned int ID;
+
+};
+
+#endif // !program_h
--- Cloth-Simplification/main.cpp
+++ Cloth-Simplification/main.cpp
@@ -1,27 +1,20 @@
-﻿#include <iostream>
+﻿
 
-// GLAD
+#include <iostream>
 #include <glad/glad.h>
-
-// GLFW
 #include <GLFW/glfw3.h>
-
+#define STB_IMAGE_IMPLEMENTATION
 #include "lim/model.h"    
 
+const GLuint WIDTH = 800, HEIGHT = 600;
 
-// This example is taken from https://learnopengl.com/
-// https://learnopengl.com/code_viewer.php?code=getting-started/hellowindow2
-// The code originally used GLEW, I replaced it with Glad
-
-// Compile:
-// g++ example/c++/hellowindow2.cpp -Ibuild/include build/src/glad.c -lglfw -ldl
-
-
-// Function prototypes
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+     std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 // Window dimensions
-const GLuint WIDTH = 800, HEIGHT = 600;
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
@@ -54,33 +47,19 @@ int main()
         return -1;
     }
 
-    // Define the viewport dimensions
     glViewport(0, 0, WIDTH, HEIGHT);
 
-    // Game loop
     while (!glfwWindowShouldClose(window))
     {
-        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
         glfwPollEvents();
 
-        // Render
-        // Clear the colorbuffer
+       
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap the screen buffers
         glfwSwapBuffers(window);
     }
 
-    // Terminates GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
-}
-
-// Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    std::cout << key << std::endl;
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
 }
