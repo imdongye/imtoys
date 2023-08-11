8c9c27e5f0e4724af53449bf6defb053d81de00e
imdongye <songjo35@naver.com>
2023-08-12 00:17:58 +0900

    rm assimp, glfw; add assimp-mdb

--- vendor/assimp-5.2.5/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/workspace/SDK/assimp/contrib/zlib
-# Build directory: E:/workspace/SDK/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
